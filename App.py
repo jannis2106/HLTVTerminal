76a27ee027d9fe1378d0bbd3564c7770f76f1f76
currentScript <jannisull@gmail.com>
2022-10-21 18:33:35 +0200

    updated path, removed comments

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import Data
 import json
+import os
 
 upcomingMatches = []
 previousMatches = []
@@ -20,19 +21,5 @@ team = {
     "previousMatches": previousMatches
 }
 
-# print(team)
-
-with open("data.json", "w", encoding="utf-8") as f:
+with open(os.path.expanduser("~") + "/.config/hltvdata.json", "w", encoding="utf-8") as f:
     json.dump(team, f, ensure_ascii=False, indent=2)
-
-# print(previousMatches)
-
-# print(name)
-# print(ranking + "    Winstreak: " + winstreak + "    WR: " + winrate + "    top30: " + top30 + "    avg age: " + avgage)
-
-# print("---------------------------------------------------------------------------------------------------------")
-# print(previousMatches[0]["enemy"] + "\t\t" + previousMatches[1]["enemy"] + "\t\t" + previousMatches[2]["enemy"] + "\t\t" + previousMatches[3]["enemy"] + "\t\t" + previousMatches[4]["enemy"])
-# print(previousMatches[0]["score"] + "\t\t" + previousMatches[1]["score"] + "\t\t" + previousMatches[2]["score"] + "\t\t" + previousMatches[3]["score"] + "\t\t\t" + previousMatches[4]["score"])
-# print("---------------------------------------------------------------------------------------------------------")
-# print(upcomingMatches[0]["enemy"] + "\t\t" + upcomingMatches[1]["enemy"] + "\t\t" + upcomingMatches[2]["enemy"] + "\t\t" + upcomingMatches[3]["enemy"] + "\t\t" + upcomingMatches[4]["enemy"])
-# print(upcomingMatches[0]["date"] + "\t\t" + upcomingMatches[1]["date"] + "\t\t" + upcomingMatches[2]["date"] + "\t\t" + upcomingMatches[3]["date"] + "\t\t\t" + upcomingMatches[4]["date"])
